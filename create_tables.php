891d655ba84896130175bff9fe697e58e58743af
Kurt Wilson <kurt4wilson@gmail.com>
2021-01-13 20:03:42 -0500

    add some notes

--- create_tables.php
+++ create_tables.php
@@ -1,15 +1,28 @@
 <?php
-$username="admin";
-$password=getenv("RDS_PASSWORD");
-$database=getenv("RDS_DB_NAME");
-$host=getenv("RDS_HOSTNAME");
-$port=getenv("RDS_PORT");
+// 1. connect to the database - done
+// 2. check if tables exist in correct format - TODO
+// 3. create (or update) tables to match required schema - TODO
+
+// TODO - move connection info and object to own file
+$username = "admin";
+
+// if running locally, set these environment variables accordingly
+
+// never, ever put a password in code. instead, read it from either a file, or an environment variable
+$password = getenv("RDS_PASSWORD");
+
+$database = getenv("RDS_DB_NAME");
+$host = getenv("RDS_HOSTNAME");
+$port = getenv("RDS_PORT");
+
+// connect to database
 $mysqli = new mysqli($host, $username, $password, $database, $port);
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-}else{
-    echo "success connecting";
+    return;
+} else {
+    echo "success connecting!";
 }
 
-?>
+// check tables and update if necessary
