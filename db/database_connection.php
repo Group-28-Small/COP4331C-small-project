a81dcc7d201d90d5da5fa2ccd33d6447a3ed1bd4
Kurt Wilson <kurt4wilson@gmail.com>
2021-01-18 13:19:10 -0500

    add create and get user functions

--- db/database_connection.php
+++ db/database_connection.php
@@ -3,6 +3,10 @@ class DBConnection
 {
     private $connection;
     private $connected = false;
+
+    private $get_user_by_username_statement;
+    private $create_user_statement;
+
     function __construct()
     {
         $username = "admin";
@@ -25,6 +29,9 @@ class DBConnection
         } else {
             $this->connected = true;
         }
+
+        $this->get_user_by_username_statement = $this->connection->prepare("SELECT (user_id, user_username, user_first_name, user_last_name, created_at, last_on) FROM (users) WHERE username=?");
+        $this->create_user_statement = $this->connection->prepare("INSERT INTO users (user_password, user_username, user_first_name, user_last_name) VALUES (?, ?, ?, ?) ");
     }
 
     function is_connected()
@@ -38,5 +45,24 @@ class DBConnection
             throw new Exception("not connected!");
         }
         return $this->connection->query($query);
+    }
+
+    function get_user_by_username($user_name)
+    {
+        $this->get_user_by_username_statement->bind_param("s", $user_name);
+        $this->get_user_by_username_statement->execute();
+        $result = $this->get_user_by_username_statement->get_result();
+        return $result;
+    }
+
+    function create_user($username, $password, $firstname, $lastname)
+    {
+        $this->create_user_statement->bind_param("ssss", $username, $password, $firstname, $lastname);
+        $this->get_user_by_id_statement->execute();
+
+        $this->get_user_by_username_statement->bind_param("s", $username);
+        $this->get_user_by_username_statement->execute();
+        $result = $this->get_user_by_username_statement->get_result();
+        return $result;
     }
 }
