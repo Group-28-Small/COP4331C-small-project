9ccbdea904f74a0a55e1e468202643ba37fd406f
Kurt Wilson <kurt4wilson@gmail.com>
2021-01-18 11:58:53 -0500

    create database layout

--- create_tables.php
+++ create_tables.php
@@ -23,6 +23,7 @@ if ($mysqli->connect_errno) {
     return;
 } else {
     echo "<strong>success connecting!</strong>";
+    // echo var_dump($mysqli->query("SELECT DATABASE()")->fetch_all());
 }
 
 // check tables and update if necessary
@@ -34,17 +35,23 @@ $mysqli->query("
         user_username VARCHAR(255) UNIQUE NOT NULL,
         user_first_name VARCHAR(255) NOT NULL,
         user_last_name VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        last_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )  ENGINE=INNODB; 
 ");
 
-// $mysqli->query("
-//     CREATE TABLE IF NOT EXISTS users (
-//         user_id INT AUTO_INCREMENT PRIMARY KEY,
-//         user_username VARCHAR(255) UNIQUE NOT NULL,
-//         user_first_name VARCHAR(255) NOT NULL,
-//         user_last_name VARCHAR(255) NOT NULL,
-//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//     )  ENGINE=INNODB; 
-// ");
+$mysqli->query(
+    "
+    CREATE TABLE IF NOT EXISTS contacts (
+        contact_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_first_name VARCHAR(255) NOT NULL,
+        user_last_name VARCHAR(255),
+        user_phone VARCHAR(255),
+        user_email VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        owner INT,
+        FOREIGN KEY (owner) REFERENCES users(user_id)
+    )  ENGINE=INNODB; 
+"
+);
 $mysqli->close();
