b3ea59f977848fb8d7ecc475552cb170a9c90703
Brian Malyszka <brian.maly@knights.ucf.edu>
2021-01-20 19:53:53 -0500

    added files from login and registration page

--- registration/newaccount.php
+++ registration/newaccount.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Brian's Testing Website</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../styles.css">
   </head>
   <body>
     <header>
@@ -11,17 +11,27 @@
     </header>
     <br><br><br><br>
     <div class="container">
-      Login
-      <form action="api/account/login.php">
+      Enter New Account:
+      <form action = "register.php" method="get">
+        <p><span class="error">* required field</span></p>
+        <label for="firstName">First Name:</label>
+        <input type="text" id="firstName" name="first">
+        <span class="error">*</span>
+        <br><br>
+        <label for="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="last">
+        <span class = "error">*</span>
+        <br><br>
         <label for="username">Username:</label>
         <input type="text" id="username" name="user">
+        <span class = "error">*</span>
         <br><br>
         <label for="password">Password:</label>
         <input type="password" id="password" name="pass">
+        <span class = "error">*</span>
         <br><br>
         <input type="submit" value="Submit" style="padding-right: 6px;align:center;">
         <br>
-        <a href="http://cop4331small-env.eba-tiprek3s.us-east-1.elasticbeanstalk.com/registration/newaccount.php" style="font-size:16px;">Don't have an account? Click here!</a>
       </form>
     </div>
   </body>
